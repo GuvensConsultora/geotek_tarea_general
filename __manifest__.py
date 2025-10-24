6ba35ff17847411008d93a3977fdd8621288c4f3
GuvensConsultora <guvensconsultora@gmail.com>
2025-10-24 19:09:36 -0300

    actualizar vista

--- __manifest__.py
+++ __manifest__.py
@@ -4,7 +4,7 @@
     "author": "Tu Empresa",
     "license": "LGPL-3",
     "category": "Project",
-    "depends": ["base", "project", "web","industry_fsm_report"],
+    "depends": ["base", "project", "web","studio_custimization"],
     "data": [
         "views/worksheet_button.xml",
         #"data/worksheet_automation.xml"
